5929906e6a020c0b67e5bae04cd70d29ebaecd8f
hmalik144 <h.malik144@gmail.com>
2022-10-30 14:18:34 +0000

    - lint fix

--- lib/main.dart
+++ lib/main.dart
@@ -86,7 +86,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
+      home: const HomePage(),
     );
   }
 }
